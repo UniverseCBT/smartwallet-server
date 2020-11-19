96982f96bdb0965c79b0bddab0b12f4e504f2044
NakaokaGabriel <gabrielnakaoka_99@hotmail.com>
2020-11-18 23:12:41 -0300

    feature: create base to sessions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import usersRouter from './users.routes';
+import sessionRouter from './session.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+
+routes.use('/sessions', sessionRouter);
 
 export default routes;
