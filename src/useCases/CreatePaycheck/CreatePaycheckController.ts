5f3ddf044172de89ddd8e367e43993eef851845b
Nakaoka <40530475+NakaokaGabriel@users.noreply.github.com>
2020-11-25 22:42:30 -0300

    Revert "feature: update user wallet structure base"

--- src/useCases/CreatePaycheck/CreatePaycheckController.ts
+++ src/useCases/CreatePaycheck/CreatePaycheckController.ts
@@ -3,8 +3,6 @@ import { Request, Response } from 'express';
 import { PaycheckRepository } from '../../repositories/paycheck/database/PaycheckRepository';
 import { CreatePaycheckUseCase } from './CreatePaycheckUseCase';
 
-import { UpdateUserWallet } from '../UpdateUserWallet/UpdateUserWallet';
-
 class CreatePaycheckController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, wallet } = request.body;
@@ -13,19 +11,13 @@ class CreatePaycheckController {
     const paycheckRepository = new PaycheckRepository();
     const createPaycheckUseCase = new CreatePaycheckUseCase(paycheckRepository);
 
-    const updateUserWalletUseCase = new UpdateUserWallet();
-
     const paycheck = await createPaycheckUseCase.execute({
       name,
       wallet,
       user_id: id,
     });
 
-    const walletUpdate = await updateUserWalletUseCase.execute({
-      user: id,
-    });
-
-    return response.status(200).json({ paycheck, walletUpdate });
+    return response.status(200).json(paycheck);
   }
 }
 
--- src/useCases/UpdateUserWallet/UpdateUserWallet.spec.ts
+++
@@ -1,3 +0,0 @@
-describe('UpdateUserWallet', () => {
-  it('should be able update a user on create a new paycheck wallet', async () => {});
-});
