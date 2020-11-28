9466a18b5e105e9c41b9ae20121c4b36e9fda2ab
NakaokaGabriel <gabrielnakaoka_99@hotmail.com>
2020-11-28 19:00:53 -0300

    feature: add delete paycheck base and implementation

--- src/repositories/paycheck/IPaycheckRepository.ts
+++ src/repositories/paycheck/IPaycheckRepository.ts
@@ -1,7 +1,14 @@
+import { DeleteResult } from 'typeorm';
+
 import { Paycheck } from '../../entities/Paycheck';
 import { ICreatePaycheckDTO } from '../../useCases/CreatePaycheck/ICreatePaycheckDTO';
 
 export interface IPaycheckRepository {
   create(data: ICreatePaycheckDTO): Promise<Paycheck>;
   findByName(name: string, user_id: string): Promise<Paycheck | undefined>;
+  findWallet(
+    paycheck_id: string,
+    user_id: string,
+  ): Promise<Paycheck | undefined>;
+  delete(paycheck_id: string): Promise<DeleteResult>;
 }
---
+++ src/useCases/DeletePaycheck/DeletePaycheckUseCase.ts
@@ -0,0 +1,43 @@
+import { AppError } from '../../share/AppError';
+
+import { IPaycheckRepository } from '../../repositories/paycheck/IPaycheckRepository';
+import { IUsersRepository } from '../../repositories/users/IUsersRepository';
+
+interface Request {
+  paycheck_id: string;
+  user_id: string;
+}
+
+export class DeletePaycheckUseCase {
+  constructor(
+    private paycheckRepository: IPaycheckRepository,
+
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ paycheck_id, user_id }: Request): Promise<void> {
+    const userPaycheckWallet = await this.paycheckRepository.findWallet(
+      paycheck_id,
+      user_id,
+    );
+
+    if (!userPaycheckWallet) {
+      throw new AppError('User does not exist with this paycheck');
+    }
+
+    const user = await this.usersRepository.findByUserId(user_id);
+
+    if (!user) {
+      throw new AppError('User does not exist');
+    }
+
+    const walletLess = user.wallet - userPaycheckWallet.wallet;
+
+    await this.usersRepository.updateWallet({
+      ...user,
+      wallet: walletLess,
+    });
+
+    await this.paycheckRepository.delete(paycheck_id);
+  }
+}
