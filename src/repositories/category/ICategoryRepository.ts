008b1e3cd4e12830364fa4267ac9bbd47031b6ef
NakaokaGabriel <gabrielnakaoka_99@hotmail.com>
2020-12-05 01:28:12 -0300

    Revert "implementation category and validate"
    This reverts commit 4d9c6177925a9ac1a8947cdf17f092e02c7d84d3.

--- src/repositories/category/ICategoryRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Category } from '../../entities/Category';
-import { ICreateCategoriesDTO } from '../../useCases/_admin/CreateCategory/ICreateCategoriesDTO';
-
-export interface ICategoryRepository {
-  create(data: ICreateCategoriesDTO): Promise<Category>;
-}
--- src/useCases/_admin/CreateCategory/CreateCategoryController.ts
+++ src/useCases/_admin/CreateCategory/CreateCategoryController.ts
@@ -1,29 +1,10 @@
 import { Request, Response } from 'express';
 
-import { CategoryRepository } from '../../../repositories/category/database/CategoryRepository';
-import { CreateCategoriesUseCase } from './CreateCategoriesUseCase';
-
-import { UsersRepository } from '../../../repositories/users/database/UsersRepository';
-
 class CreateCategoryController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { category, description, importance } = request.body;
 
-    const categoryRepository = new CategoryRepository();
-    const usersRepository = new UsersRepository();
-
-    const createCategory = new CreateCategoriesUseCase(
-      categoryRepository,
-      usersRepository,
-    );
-
-    const categoryResult = await createCategory.execute({
-      category,
-      description,
-      importance,
-    });
-
-    return response.status(200).json(categoryResult);
+    return response.status(200).json({ ok: true });
   }
 }
 
