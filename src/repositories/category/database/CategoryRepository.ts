c305a59eedf6aff6968466e0300d4c737af6ad28
NakaokaGabriel <gabrielnakaoka_99@hotmail.com>
2020-12-05 01:32:41 -0300

    Revert "Revert "implementation category and validate""
    This reverts commit 008b1e3cd4e12830364fa4267ac9bbd47031b6ef.

---
+++ src/repositories/category/database/CategoryRepository.ts
@@ -0,0 +1,17 @@
+import { getRepository } from 'typeorm';
+
+import { Category } from '../../../entities/Category';
+import { ICreateCategoriesDTO } from '../../../useCases/_admin/CreateCategory/ICreateCategoriesDTO';
+import { ICategoryRepository } from '../ICategoryRepository';
+
+export class CategoryRepository implements ICategoryRepository {
+  private ormRepository = getRepository(Category);
+
+  public async create(data: ICreateCategoriesDTO): Promise<Category> {
+    const category = this.ormRepository.create(data);
+
+    await this.ormRepository.save(category);
+
+    return category;
+  }
+}
