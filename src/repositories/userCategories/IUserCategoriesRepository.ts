07a45b88a05afd3f404a4a99bb3ed2b6afcc28f6
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-12-06 00:02:12 -0300

    remove user category repository

--- src/repositories/userCategories/IUserCategoriesRepository.ts
+++
@@ -1,6 +0,0 @@
-import { UserCategories } from '../../entities/UserCategories';
-import { ICreateUserCategoriesDTO } from '../../useCases/CreateUserCategory/ICreateUserCategoriesDTO';
-
-export interface IUserCategoriesRepository {
-  create(data: ICreateUserCategoriesDTO): Promise<UserCategories>;
-}
