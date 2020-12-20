163c58727d3579d97678e367b62c1f2c28c52c44
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-12-20 19:03:28 -0300

    add wallet and income table

---
+++ src/database/migrations/1608500427514-CreateUserIncome.ts
@@ -0,0 +1,70 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateUserIncome1608500427514
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'incomes',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'expected_money',
+            type: 'decimal',
+            default: 0,
+          },
+          {
+            name: 'current_money',
+            type: 'decimal',
+            default: 0,
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'incomes',
+      new TableForeignKey({
+        name: 'UserIncome',
+        columnNames: ['user_id'],
+        referencedTableName: 'users',
+        referencedColumnNames: ['id'],
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('incomes', 'UserIncome');
+
+    await queryRunner.dropTable('incomes');
+  }
+}
