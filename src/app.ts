830ec0f12996de789761a233b65ecb9faae20f4f
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2021-07-05 21:32:57 -0300

    enable cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -9,6 +10,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
@@ -20,6 +22,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     });
   }
 
+  // eslint-disable-next-line no-console
   console.error(err);
 
   return response.status(500).json({
